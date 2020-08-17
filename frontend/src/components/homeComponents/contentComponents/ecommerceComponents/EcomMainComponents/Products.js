e6223c4795de6a61983136291fcddd39bcfcee6e
franco0621 <francis.june21@gmail.com>
2020-08-17 23:20:12 +0800

    add-redux-to-filter-component

--- frontend/src/components/homeComponents/contentComponents/ecommerceComponents/EcomMainComponents/Products.js
+++ frontend/src/components/homeComponents/contentComponents/ecommerceComponents/EcomMainComponents/Products.js
@@ -119,6 +119,6 @@ class Products extends Component {
         )
     }
 }
-export default connect((state) => ({products: state.products.items}),{fetchProducts})(Products);
+export default connect((state) => ({products: state.products.filteredItems}),{fetchProducts})(Products);
 
 
