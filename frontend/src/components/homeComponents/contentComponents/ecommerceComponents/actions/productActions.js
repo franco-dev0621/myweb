bac88b91ef8e5f692a8b17fae029f9a41494c73d
franco0621 <francis.june21@gmail.com>
2020-08-17 20:41:38 +0800

    add-redux-products

---
+++ frontend/src/components/homeComponents/contentComponents/ecommerceComponents/actions/productActions.js
@@ -0,0 +1,12 @@
+import { FETCH_PRODUCTS } from "../types"
+
+
+export const fetchProducts = () => async(dispatch) => {
+    const res = await fetch("http://localhost:5001/products/")
+    const data = await res.json();
+    console.log(data);
+    dispatch ({
+        type: FETCH_PRODUCTS,
+        payload: data,
+    });
+};
